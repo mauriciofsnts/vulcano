e443b5337566d74815f8f90abc589adbeaea85c9
Mauricio Ferraz <mauriciofsnts@gmail.com>
2023-10-29 15:45:48 -0300

    feat: short links

---
+++ internal/utils/shortner.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+const endpoint = "https://st.mrzt.dev/api/v2/links"
+
+type Response struct {
+	Address     string
+	Banned      bool
+	Created_at  string
+	Id          string
+	Link        string
+	Password    bool
+	Target      string
+	Description string
+	Updated_at  string
+	Visit_count int
+}
+
+func Shortner(url string) (string, error) {
+
+	requestBody, err := json.Marshal(map[string]string{
+		"target":       url,
+		"showAdvanced": "false",
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	res, err := http.Post(endpoint, "application/json", bytes.NewBuffer(requestBody))
+
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	var response Response
+
+	err = json.Unmarshal(body, &response)
+
+	if err != nil {
+		return "", err
+	}
+
+	return response.Link, nil
+}
---
+++ internal/utils/shortner_test.go
@@ -0,0 +1,23 @@
+package utils_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/mauriciofsnts/vulcano/internal/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShortner(t *testing.T) {
+	// Create a test server to mock the HTTP endpoint
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"link": "https://short.link/abc123"}`))
+	}))
+	defer testServer.Close()
+
+	// Call the Shortner function with the test server URL
+	shortLink, err := utils.Shortner(testServer.URL)
+	assert.NoError(t, err)
+	assert.Contains(t, shortLink, "https://st.mrzt.dev/")
+}
