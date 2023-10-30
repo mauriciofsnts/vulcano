a63364c6f867f8487b32f15c82f6d8ac914e4f88
Mauricio Ferraz <mauriciofsnts@gmail.com>
2023-10-30 11:31:59 -0300

    feat: show bot ping on ping command

--- internal/discord/commands/utils/ping.go
+++ internal/discord/commands/utils/ping.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/mauriciofsnts/vulcano/internal/discord/events"
 )
@@ -8,11 +10,42 @@ import (
 func init() {
 	events.Register("ping", events.CommandInfo{
 		Function: func(cm events.CommandMessage) {
-			cm.Ok(&discordgo.MessageEmbed{Description: cm.T.Commands.Ping.Response.Str()})
+			latency := cm.Session.HeartbeatLatency().Milliseconds()
+			ms := formatAPILatency(latency)
+
+			cm.Ok(&discordgo.MessageEmbed{
+				Title: "Pong!",
+				Fields: []*discordgo.MessageEmbedField{
+					{
+						Name:   "API Latency",
+						Value:  ms,
+						Inline: true,
+					},
+				},
+			})
+
 		},
 		ApplicationCommand: &discordgo.ApplicationCommand{
 			Name:        "ping",
 			Description: "Pong!",
 		},
 	})
+}
+
+func formatAPILatency(latency int64) string {
+	ms := latency
+
+	var icon string
+	if ms < 50 {
+		icon = "🟢"
+	} else if ms < 100 {
+		icon = "🟡"
+	} else {
+		icon = "🔴"
+	}
+	return fmt.Sprintf(
+		"%s %d ms",
+		icon,
+		ms,
+	)
 }
