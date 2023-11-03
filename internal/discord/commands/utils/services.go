cfe54f80b683b5ab69448039b6ad4ce348c3c947
Mauricio Ferraz <mauriciofsnts@gmail.com>
2023-11-03 17:47:36 -0300

    feat: services command

---
+++ internal/discord/commands/utils/services.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/mauriciofsnts/vulcano/internal/discord/bot"
+)
+
+func init() {
+	bot.RegisterCommand(
+		"services",
+		bot.Command{
+			Name:        "services",
+			Aliases:     []string{"services"},
+			Description: "List of open services",
+			Handler: func(ctx *bot.Context) {
+
+				embed := discord.Embed{
+					Title: "Open Services",
+					URL:   "https://github.com/mauriciofsnts",
+					Fields: []discord.EmbedField{
+						{
+							Name:   "Encurtador de URL",
+							Value:  "https://st.mrzt.dev/",
+							Inline: false,
+						},
+						{
+							Name:   "Uptime",
+							Value:  "https://uptime.mrzt.dev/",
+							Inline: false,
+						},
+						{
+							Name:   "Web Development",
+							Value:  "https://speedify.dev/",
+							Inline: false,
+						},
+					},
+				}
+
+				ctx.Reply(embed)
+			},
+		},
+	)
+}
