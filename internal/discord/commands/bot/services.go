51edce2f6130c4b4ccf299925c1e5c9716b7e253
Mauricio Ferraz <mauriciofsnts@gmail.com>
2024-07-27 20:30:30 -0300

    fix:  services command

--- internal/discord/commands/bot/services.go
+++ internal/discord/commands/bot/services.go
@@ -11,14 +11,15 @@ func init() {
 		Aliases:     []string{"services"},
 		Description: "List of services available",
 		Options:     []discord.ApplicationCommandOption{},
-		Handler: func(ctx *ctx.Context) discord.MessageCreate {
+		Handler: func(ctx *ctx.Context) *discord.MessageCreate {
 
 			fields := []discord.EmbedField{
 				{Name: "Squarefox", Value: "https://squarefox.digital/"},
 				{Name: "Website", Value: "https://mrtz.dev/"},
 			}
 
-			return ctx.Reply(ctx.Embed("Services", "Here is a list of services available", fields))
+			reply := ctx.Build(ctx.Embed("Services", "Here is a list of services available", fields))
+			return &reply
 		},
 	})
 }
