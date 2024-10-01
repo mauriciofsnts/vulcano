8fe371653e7a99c4e1df17825d7125419265ab13
Mauricio Ferraz <mauriciofsnts@gmail.com>
2024-10-01 13:33:19 -0300

    feat: enable server

--- internal/internal.go
+++ internal/internal.go
@@ -3,12 +3,13 @@ package internal
 import (
 	"github.com/mauriciofsnts/bot/internal/config"
 	"github.com/mauriciofsnts/bot/internal/discord"
+	"github.com/mauriciofsnts/bot/internal/server"
 )
 
 func Bootstrap() {
 	config.ConfigLogger()
 	config.Init()
 
-	// go server.StartHttpServer()
+	go server.StartHttpServer()
 	discord.Init()
 }
--- internal/server/handler/health.go
+++ internal/server/handler/health.go
@@ -6,4 +6,5 @@ import (
 
 func Health(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
